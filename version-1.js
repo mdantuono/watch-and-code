dcf849c1cb5d4ebc2aa2c7c6c6ecfc6b1235591c
Michael DAntuono <mikeishere3@gmail.com>
2017-11-08 17:02:46 +0000

    Version 1

---
+++ version-1.js
@@ -0,0 +1,10 @@
+// Store todos
+var todos = ['Item 1','Item 2','Item 3','Item 4'];
+// Display todos
+console.log("My Todos:", todos);
+// Add new todos
+todos.push('Item 5');
+//Change a todo
+todos[0] = 'Item 1 changed';
+//Delete a todo
+todos.splice(0, 1);
